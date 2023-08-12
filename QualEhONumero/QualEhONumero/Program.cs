fc635e8d21e38982f0f0faaa1cf73a720cc4a0e8
Joel-Nogueira <joelnogueira733@gmail.com>
2023-08-12 10:55:00 -0300

    Criação do projeto Qual é o número

---
+++ QualEhONumero/QualEhONumero/Program.cs
@@ -0,0 +1,20 @@
+﻿Random rnd = new Random();
+int numero = 0;
+
+do
+{
+    Console.WriteLine("Escolha um número de 1 a 100.");
+    numero = int.Parse(Console.ReadLine());
+
+    if (numero == rnd.Next(1, 101))
+    {
+        Console.WriteLine("Parabéns, você acertou!");
+    }
+    else
+    {
+        Console.WriteLine("Você errou o número");
+    }
+
+    Console.WriteLine($"{rnd.Next(1, 101)}");
+}
+while ( numero != -1 );
