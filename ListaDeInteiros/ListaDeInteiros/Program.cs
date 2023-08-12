3c782590963bdde6d572f3147be2783feff65e77
Joel-Nogueira <joelnogueira733@gmail.com>
2023-08-12 13:43:54 -0300

    Criação do projeto NumeroEhPar

---
+++ ListaDeInteiros/ListaDeInteiros/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+void ExibirNumeroSeForPar(int numero)
+{
+    bool ehPar = (numero % 2) == 0;
+    if (ehPar)
+    {
+        Console.WriteLine(numero);
+    }
+}
+
+void ExibirLista()
+{
+    List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    
+
+    Console.WriteLine("FOR");
+    for (int i = 0; i < numeros.Count; i++)
+    {
+        ExibirNumeroSeForPar(numeros[i]);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("FOREACH"); 
+
+    foreach (int numero in numeros)
+    {
+        ExibirNumeroSeForPar(numero);
+    }
+}
+
+ExibirLista();
