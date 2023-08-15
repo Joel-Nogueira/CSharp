c09fe9cfb3876d00a29275de7e313365b09b9703
Joel-Nogueira <joelnogueira733@gmail.com>
2023-08-15 15:19:55 -0300

    Criação do projeto Podcast

---
+++ Podcast/Podcast/Episodio.cs
@@ -0,0 +1,20 @@
+﻿class Episodio
+{
+    List<string> _convidados = new();
+
+    public Episodio(int duracao = 0, int numero = 0, string titulo = null)
+    {
+        Duracao = duracao;
+        Numero = numero;
+        Titulo = titulo;
+    }
+    public int Duracao { get; }
+    public int Numero { get; }
+    public string Resumo => $"{Numero}: {Titulo} - Duração: {Duracao} min {string.Join(", ", _convidados)}";
+    public string Titulo { get; }
+
+    public void AdicionarConvidados(string convidado)
+    {
+        _convidados.Add(convidado);
+    }
+}
---
+++ Podcast/Podcast/Program.cs
@@ -0,0 +1,20 @@
+﻿Episodio episodio1 = new Episodio(100, 99, "Nove nove");
+episodio1.AdicionarConvidados("Joel");
+episodio1.AdicionarConvidados("Lu");
+episodio1.AdicionarConvidados("Alan");
+
+Episodio episodio2 = new Episodio(100, 33, "Trinta e três");
+episodio2.AdicionarConvidados("Gleryston");
+episodio2.AdicionarConvidados("João");
+episodio2.AdicionarConvidados("Yuri");
+
+Episodio episodio3 = new Episodio(100, 88, "Oito oito");
+episodio3.AdicionarConvidados("Raimundo");
+episodio3.AdicionarConvidados("Leonardo");
+episodio3.AdicionarConvidados("Alan");
+
+Podcast podcast = new Podcast("Joel", "Podcast do Joel");
+podcast.AdicionarEpisodio(episodio1);
+podcast.AdicionarEpisodio(episodio2);
+podcast.AdicionarEpisodio(episodio3);
+podcast.ExibirDetalhes();
