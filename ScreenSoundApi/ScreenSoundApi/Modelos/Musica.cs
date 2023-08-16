fe3d6742854382d9fd1bc94c79685c22b0952372
Joel-Nogueira <joelnogueira733@gmail.com>
2023-08-15 21:07:53 -0300

    Criação do projeto ScreenSoundApi

---
+++ ScreenSoundApi/ScreenSoundApi/Modelos/Musica.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ScreenSoundApi.Modelos;
+
+class Musica
+{
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhesDaMusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Música: {Nome}");
+        Console.WriteLine($"Duracao: {Duracao / 1000}");
+        Console.WriteLine($"Gênero: {Genero}");
+    }
+}
---
+++ ScreenSoundApi/ScreenSoundApi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using ScreenSoundApi.Modelos;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[0].ExibirDetalhesDaMusica();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
