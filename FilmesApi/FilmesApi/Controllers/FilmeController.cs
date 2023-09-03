96f64c7e09be25ff97c21d9b8a7cee8ec2077b54
Joel-Nogueira <joelnogueira733@gmail.com>
2023-09-03 20:34:45 -0300

    Criação do controller e adição de novos métodos

---
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FilmesApi.Models;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int _id = 0;
+
+    [HttpPost]
+    public IActionResult AdicionarFilme([FromBody] Filme filme)
+    {
+        filme.Id = _id++;
+        filmes.Add(filme);
+        return CreatedAtAction(nameof(RecuperarFilmePorId),
+            new { id = filme.Id},
+            filme);
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> RecuperarTodosOsFilmes([FromQuery] int skip = 0, [FromQuery] int take = 10)
+    {
+        return filmes.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperarFilmePorId(int id)
+    {
+        Filme filme = filmes.FirstOrDefault(f => f.Id == id);
+
+        if (filme == null) 
+            return NotFound();
+
+        return Ok(filme);
+    }
+}
---
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O título do filme não pode ser vazio.")]
+    [MaxLength(50, ErrorMessage = "O título do filme não pode exceder 50 caracteres.")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O gênero do filme não pode exceder 50 caracteres")]
+    public string Genero { get; set; }
+
+    [Range(90, 180, ErrorMessage = "A duração deve estar entre 90 a 180 minutos")]
+    public int Duracao { get; set; }
+
+}
