75a4629fb5ebb78ff1e8cf5a15677f8ddaeee57d
Joel-Nogueira <joelnogueira733@gmail.com>
2023-09-07 14:09:41 -0300

    Exercício de criação de api de Músicas=

---
+++ ExercicioApi/ExercicioApi/Controllers/MusicaController.cs
@@ -0,0 +1,37 @@
+﻿using ExercicioApi.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace ExercicioApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MusicaController
+{
+    private static List<Musica>? _musicas;
+
+    [HttpPost]
+    public async void BuscarMusicasNaUrl([FromBody] string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            string resposta = await client.GetStringAsync(url);
+            _musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);
+        }
+    }
+
+    [HttpGet]
+    public List<Musica>? RetornarTodasMusicas()
+    {
+        return _musicas;
+    }
+
+    [HttpGet("ano/{ano}")]
+    public IEnumerable<Musica> GetMusicasPorAno(string? ano)
+    {
+        if (_musicas == null)
+            return null;
+
+        return _musicas.Where(m => m.Ano.Equals(ano));
+    }
+}
---
+++ ExercicioApi/ExercicioApi/Modelos/Musica.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExercicioApi.Modelos;
+
+public class Musica
+{
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; }
+
+    [JsonPropertyName("year")]
+    public string Ano { get; set; }
+
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+}
