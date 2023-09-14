8ef90899b9adb1404de97840d61560babf47872a
Joel-Nogueira <joelnogueira733@gmail.com>
2023-09-14 18:12:25 -0300

    Inclusão da entidade Cinema

---
+++ FilmesApi/FilmesApi/Data/CinemaContext.cs
@@ -0,0 +1,15 @@
+﻿using FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data;
+
+public class CinemaContext : DbContext
+{
+    public CinemaContext(DbContextOptions<CinemaContext> opts)
+        : base(opts)
+    {
+        
+    }
+
+    
+}
